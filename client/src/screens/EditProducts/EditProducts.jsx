3cf41ccaa9419256b780cdd2c93883b5e0c77c3e
haydo <haydent2013is10@gmail.com>
2022-01-03 11:39:18 -0700

    Throw away test

--- client/src/screens/EditProducts/EditProducts.jsx
+++ client/src/screens/EditProducts/EditProducts.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function EditProducts() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default EditProducts
+export default EditProducts;
