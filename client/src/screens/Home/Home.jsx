55fa0ed0032cca6c6d7184e20e8caef3330cdbc1
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-01-06 10:46:36 -0600

    linked carousel to detail page

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -36,107 +36,167 @@ function Home(props) {
           <CarouselItem>
             {" "}
             <div className="main-container-items">
-              <img
-                src={props.products[2]?.imgURL}
-                alt={props.products[2]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[2]?.name}</h4>
+              <Link
+                to={`/products/${props.products[2]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[2]?.imgURL}
+                  alt={props.products[2]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[2]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[0]?.imgURL}
-                alt={props.products[0]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[0]?.name}</h4>
+              <Link
+                to={`/products/${props.products[0]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[0]?.imgURL}
+                  alt={props.products[0]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[0]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[8]?.imgURL}
-                alt={props.products[8]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[8]?.name}</h4>
+              <Link
+                to={`/products/${props.products[8]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[8]?.imgURL}
+                  alt={props.products[8]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[8]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items media">
-              <img
-                src={props.products[15]?.imgURL}
-                alt={props.products[15]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[15]?.name}</h4>
+              <Link
+                to={`/products/${props.products[15]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[15]?.imgURL}
+                  alt={props.products[15]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[15]?.name}</h4>
+              </Link>
             </div>
           </CarouselItem>
           <CarouselItem>
             <div className="main-container-items">
-              <img
-                src={props.products[18]?.imgURL}
-                alt={props.products[18]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[18]?.name}</h4>
+              <Link
+                to={`/products/${props.products[18]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[18]?.imgURL}
+                  alt={props.products[18]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[18]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[21]?.imgURL}
-                alt={props.products[21]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[21]?.name}</h4>
+              <Link
+                to={`/products/${props.products[21]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[21]?.imgURL}
+                  alt={props.products[21]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[21]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[22]?.imgURL}
-                alt={props.products[22]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[22]?.name}</h4>
+              <Link
+                to={`/products/${props.products[22]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[22]?.imgURL}
+                  alt={props.products[22]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[22]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items media">
-              <img
-                src={props.products[32]?.imgURL}
-                alt={props.products[32]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[32]?.name}</h4>
+              <Link
+                to={`/products/${props.products[32]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[32]?.imgURL}
+                  alt={props.products[32]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[32]?.name}</h4>
+              </Link>
             </div>
           </CarouselItem>
           <CarouselItem>
             <div className="main-container-items">
-              <img
-                src={props.products[31]?.imgURL}
-                alt={props.products[31]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[31]?.name}</h4>
+              <Link
+                to={`/products/${props.products[31]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[31]?.imgURL}
+                  alt={props.products[31]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[31]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[45]?.imgURL}
-                alt={props.products[45]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[45]?.name}</h4>
+              <Link
+                to={`/products/${props.products[45]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[45]?.imgURL}
+                  alt={props.products[45]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[45]?.name}</h4>
+              </Link>
             </div>
             <div className="main-container-items media">
-              <img
-                src={props.products[47]?.imgURL}
-                alt="2018 GMC Sierra Denali Pickup"
-                className="car-items-1"
-              />
-              <h4 className="item-names-1">
-                2018 GMC Sierra <br />
-                Denali Pickup
-              </h4>
+              <Link
+                to={`/products/${props.products[47]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[47]?.imgURL}
+                  alt="2018 GMC Sierra Denali Pickup"
+                  className="car-items-1"
+                />
+                <h4 className="item-names-1">
+                  2018 GMC Sierra <br />
+                  Denali Pickup
+                </h4>
+              </Link>
             </div>
             <div className="main-container-items">
-              <img
-                src={props.products[27]?.imgURL}
-                alt={props.products[27]?.name}
-                className="car-items"
-              />
-              <h4 className="item-names">{props.products[27]?.name}</h4>
+              <Link
+                to={`/products/${props.products[27]._id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <img
+                  src={props.products[27]?.imgURL}
+                  alt={props.products[27]?.name}
+                  className="car-items"
+                />
+                <h4 className="item-names">{props.products[27]?.name}</h4>
+              </Link>
             </div>
           </CarouselItem>
         </Carousel>
